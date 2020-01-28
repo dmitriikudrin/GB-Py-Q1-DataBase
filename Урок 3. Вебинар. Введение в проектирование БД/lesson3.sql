79c21580046cfddfc8d16036a8a2bd855dd39376
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2020-01-28 20:52:08 +0700

    Update lesson3.sql

--- Урок 3. Вебинар. Введение в проектирование БД/lesson3.sql
+++ Урок 3. Вебинар. Введение в проектирование БД/lesson3.sql
@@ -5,8 +5,8 @@ USE vk;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY, -- BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT
-    firstname VARCHAR(50) COMMENT 'Имя' ,
-    lastname VARCHAR(50) COMMENT 'Фамилия',
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
     email VARCHAR(120) UNIQUE,
     `password_hash` VARCHAR(100),
     phone VARCHAR(12),
@@ -59,4 +59,75 @@ CREATE TABLE friend_requests(
     INDEX idx_target_user_id(target_user_id),
     CONSTRAINT fk_initiator_user_id FOREIGN KEY (initiator_user_id) REFERENCES users(id),
     CONSTRAINT fk_target_user_id FOREIGN KEY (target_user_id) REFERENCES users(id)
-)
+);
+
+DROP TABLE IF EXISTS communites;
+CREATE TABLE communities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150),
+
+    INDEX idx_communities_name(name)
+);
+
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+    user_id BIGINT UNSIGNED NOT NULL,
+    community_id BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (user_id, community_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (community_id) REFERENCES communities(id)
+);
+
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE media_types(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150)
+);
+
+DROP TABLE IF EXISTS media;
+CREATE TABLE media(
+    id SERIAL PRIMARY KEY,
+    media_type_id BIGINT UNSIGNED NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    body TEXT,
+    filename VARCHAR(225),
+    `size` INT,
+    metadata JSON,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    INDEX (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id)
+);
+
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    media_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT NOW(),
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (media_id) REFERENCES media(id)
+);
+
+DROP TABLE IF EXISTS photo_albums;
+CREATE TABLE photo_albums(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) DEFAULT NULL,
+    user_id BIGINT UNSIGNED DEFAULT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE photos(
+    id SERIAL PRIMARY KEY,
+    album_id BIGINT UNSIGNED,
+    media_id BIGINT UNSIGNED NOT NULL,
+
+    FOREIGN KEY (album_id) REFERENCES photo_albums(id),
+    FOREIGN KEY (media_id) REFERENCES media(id)
+);
