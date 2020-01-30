85240addaec644b503de9e186fc15dee4a516d0c
dmitriikudrin <39126785+dmitriikudrin@users.noreply.github.com>
2020-01-30 20:56:19 +0700

    Create lesson4.sql

---
+++ Урок 4. Вебинар. CRUD-операции/lesson4.sql
@@ -0,0 +1,14 @@
+USE vk;
+
+INSERT IGNORE INTO users (firstname, lastname, email, phone)
+VALUES ('Ivan', 'Pupkin', 'pupkin1@example.ru', '9231234567'),
+       ('Ivan', 'Pupkin', 'pupkin2@example.ru', '9231234567'),
+       ('Ivan', 'Pupkin', 'pupkin3@example.ru', '9231234567'),
+       ('Ivan', 'Pupkin', 'pupkin4@example.ru', '9231234567'),
+       ('Ivan', 'Pupkin', 'pupkin5@example.ru', '9231234567'),
+       ('Ivan', 'Pupkin', 'pupkin6@example.ru', '9231234567');
+
+
+INSERT INTO users (id, firstname, lastname, email, password_hash, phone)
+SELECT *
+FROM users_1;
